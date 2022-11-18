44242ad40419f2dc3fcbb998115e85dac416287a
Federico Marcattini <fedemarcattini96@gmail.com>
2022-11-18 15:50:37 -0300

    Agregando estilos

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+/*** MODAL ***/
+
+const abrirModal = document.querySelector('.header-carrito');
+const modal = document.querySelector('.modal');
+const cerrarModal = document.querySelector('.modal-cerrar');
+
+abrirModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add('modal--mostrar');
+});
+
+cerrarModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.remove('modal--mostrar');
+});
